2f07e5f73ccc813d25f3651b3f15fbe6480cfd7a
tcrurup <49696152+tcrurup@users.noreply.github.com>
2021-06-10 16:38:29 -0600

    Seperate out shaders into their own modules

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-let vertexShaderText = [
-'precision mediump float;',
-'',
-'attribute vec3 vertPosition;',
-'attribute vec3 vertColor;',
-'varying vec3 fragColor;',
-'uniform mat4 mWorld;',
-'uniform mat4 mView;',
-'uniform mat4 mProj;',
-'',
-'void main()',
-'{',
-'   fragColor = vertColor;',
-'   gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
-'}'
-].join('\n');
+import fragmentShaderText from "./webGL/shaders/fragmentShaderText.js"
+import vertexShaderText from "./webGL/shaders/vertexShaderText.js"
 
-let fragmentShaderText = [
-'precision mediump float;',
-'',
-'varying vec3 fragColor;',
-'void main()',
-'{',
-'   gl_FragColor = vec4(fragColor, 1.0);',
-'}'
-].join('\n');
 
 let initDemo = function(){
     
+    //Initiallize the GL content
     let canvas = document.getElementById("game-window");
     let gl = canvas.getContext('webgl')
+    
+    
     let mat4 = glMatrix.mat4
 
     if(!gl){
@@ -39,10 +19,10 @@ let initDemo = function(){
         alert('Your browser does not suppert WebGL')
     }
 
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
-    gl.frontFace(gl.CCW);
-    gl.cullFace(gl.BACK);
+    //gl.enable(gl.DEPTH_TEST);
+    //gl.enable(gl.CULL_FACE);
+    //gl.frontFace(gl.CCW);
+    //gl.cullFace(gl.BACK);
 
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -79,68 +59,33 @@ let initDemo = function(){
 
     let boxVertices = 
 	[ // X, Y, Z           R, G, B
-		// Top
-		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
-		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
-		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
-		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
-
-		// Left
-		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
-		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
-		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
-		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
-
-		// Right
-		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
-		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
-		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
-		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
-
 		// Front
-		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, 1.0, 0.0,    1.0, 0.0, 0.15,
+		1.0, 0.0, 0.0,    1.0, 0.0, 0.15,
+		0.0, 0.0, 0.0,    1.0, 0.0, 0.15,
+		0.0, 1.0, 0.0,    1.0, 0.0, 0.15,
 
-		// Back
-		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+        2.0, 1.0, 0.0,    0.0, 1.0, 0.15,
+		2.0, 0.0, 0.0,    0.0, 1.0, 0.15,
+		1.0, 0.0, 0.0,    0.0, 1.0, 0.15,
+		1.0, 1.0, 0.0,    0.0, 1.0, 0.15,
 
-		// Bottom
-		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
-		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
-		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
-		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
+        3.0, 1.0, 0.0,    0.0, 1.0, 0.15,
+	    3.0, 0.0, 0.0,    0.0, 1.0, 0.15,
+		2.0, 0.0, 0.0,    0.0, 1.0, 0.15,
+		2.0, 1.0, 0.0,    0.0, 1.0, 0.15,
 	];
 
     var boxIndices =
 	[
-		// Top
-		0, 1, 2,
-		0, 2, 3,
+        0, 1, 2,
+        0, 2, 3,
 
-		// Left
-		5, 4, 6,
-		6, 4, 7,
+        4, 5, 6,
+        4, 6, 7,
 
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
+        8, 9, 10, 
+        8, 10, 11
 	];
 
     let boxVertexBufferObject = gl.createBuffer();
@@ -185,32 +130,27 @@ let initDemo = function(){
     let viewMatrix = new Float32Array(16)
     let projMatrix = new Float32Array(16)
     mat4.identity(worldMatrix);
-    mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+    mat4.lookAt(viewMatrix, [0, 0, 100], [0, 0, 0], [0, 1, 0]);
     mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
 
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
     gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
-    //Main render loop
 
     let identityMatrix = new Float32Array(16);
     mat4.identity(identityMatrix);
 
-    let angle = 0;
-    let loop = function () {
-        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-        mat4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0])
-
-        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-
-        gl.clearColor(0.75, 0.85, 0.8, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
-        
-        requestAnimationFrame(loop)
-    };
-    requestAnimationFrame(loop)
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
     
 }
+
+console.log('loading script')
+document.addEventListener("DOMContentLoaded", event => {
+    console.log('DOM Loaded')
+    initDemo();
+});
---
+++ webGL/shaders/fragmentShaderText.js
@@ -0,0 +1,9 @@
+export default [
+    'precision mediump float;',
+    '',
+    'varying vec3 fragColor;',
+    'void main()',
+    '{',
+    '   gl_FragColor = vec4(fragColor, 1.0);',
+    '}'
+].join('\n');
